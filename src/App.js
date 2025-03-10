e3227bb1347f10823d3196ee3b89d9d6b137b27a
VasoyaDhruv <dhruvvasoya48@gmail.com>
2025-03-10 20:45:40 +0530

    add purchase history route and improve instructor data handling

--- src/App.js
+++ src/App.js
@@ -124,6 +124,7 @@ function App() {
           <>
           <Route path="dashboard/cart" element={<Cart />} />
           <Route path="dashboard/enrolled-courses" element={<EnrolledCourses />} />
+          <Route path="dashboard/purchase-history" element={<EnrolledCourses />} />
           </>
         )
       }
