b9c9ab0d2cdc2c06fa6da8e18a996897785fcad6
VasoyaDhruv <dhruvvasoya48@gmail.com>
2025-03-10 23:22:10 +0530

    Use optional chaining for BASE_URL fallback to support environment variable

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,5 +1,5 @@
 // const BASE_URL = process.env.REACT_APP_BASE_URL
- const BASE_URL = "https://studynotion-f9sq.onrender.com/api/v1"
+ const BASE_URL = process.env.REACT_APP_BASE_URL ?? "https://studynotion-f9sq.onrender.com/api/v1"
 
 // AUTH ENDPOINTStriny
 export const endpoints = {
